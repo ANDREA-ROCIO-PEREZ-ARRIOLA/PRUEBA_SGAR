854a951a6b6196b16b431da56e3b1bd458e779f1
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-11-08 16:39:43 -0600

    Se realizo el diseño de login de organización

--- PRUEBA FRONTEND SGAR/Program.cs
+++ PRUEBA FRONTEND SGAR/Program.cs
@@ -1,11 +1,21 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PRUEBA_FRONTEND_SGAR;
+using PRUEBA_FRONTEND_SGAR.Services; 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//CONEXIÓN DE LA API SEGURIDAD
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("http://sgarseguridad.somee.com/") //API SEGURIDAD
+});
+
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+//Servicio de autenticación
+builder.Services.AddScoped<AuthService>();
 
 await builder.Build().RunAsync();
---
+++ PRUEBA FRONTEND SGAR/Services/AuthService.cs
@@ -0,0 +1,26 @@
+﻿using PRUEBA_FRONTEND_SGAR.Models;
+using System.Net.Http.Json;
+
+namespace PRUEBA_FRONTEND_SGAR.Services
+{
+    public class AuthService
+    {
+        private readonly HttpClient _http;
+
+        public AuthService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<string?> LoginAsync(CredencialesOrganizationRequest credenciales)
+        {
+            var response = await _http.PostAsJsonAsync("api/organization/login", credenciales);
+
+            if (!response.IsSuccessStatusCode)
+                return null;
+
+            var token = await response.Content.ReadAsStringAsync();
+            return token.Replace("\"", ""); // Elimina comillas del string
+        }
+    }
+}
